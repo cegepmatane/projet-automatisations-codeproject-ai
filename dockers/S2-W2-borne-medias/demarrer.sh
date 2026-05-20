97bdb137f70381e5a29009a5281ca81d2ec5b3f3
enderbird <cedricsimard28@gmail.com>
2026-05-20 13:19:42 +0000

    demarrer

--- dockers/S2-W2-borne-medias/demarrer.sh
+++ dockers/S2-W2-borne-medias/demarrer.sh
@@ -5,7 +5,12 @@ CONTAINER="borne-medias-pygame"
 
 echo "[run] Lancement du container"
 
-xhost +local:docker
+# Vérifie si xhost existe
+if command -v xhost >/dev/null 2>&1; then
+  xhost +local:docker
+else
+  echo "[warn] xhost introuvable (installe x11-xserver-utils)"
+fi
 
 docker run -d \
   --name "$CONTAINER" \
